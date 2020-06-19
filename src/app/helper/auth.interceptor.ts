48e2d126c5304c15c42e062a47478730ad211ade
David Alejandro <davidalejandro0919@gmail.com>
2020-06-19 15:54:27 -0300

    Ajust interceptor

--- src/app/helper/auth.interceptor.ts
+++ src/app/helper/auth.interceptor.ts
@@ -9,15 +9,24 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const token = localStorage.getItem(this.tokenName);
+    if (req.url.includes('i18n')) {
+      req = req.clone({
+        setHeaders: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Accept: 'application/json'
+        },
+      });
+    } else {
+      const token = localStorage.getItem(this.tokenName);
 
-    req = req.clone({
-      setHeaders: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-    });
+      req = req.clone({
+        setHeaders: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+      });
+    }
 
     return next.handle(req);
   }
